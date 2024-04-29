1248d3720c97119775cb4ac23586df23e379613c
Abdullah Siddiki <abdullahsiddiki48@gmail.com>
2024-04-29 02:42:34 -0400

    render deploy

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+from main import main
+
+def application(environ, start_response):
+    path = environ.get('PATH_INFO', '').lstrip('/')
+    if path == 'health':
+        start_response('200 OK', [('Content-Type', 'text/plain')])
+        return [b"OK"]
+    else:
+        main()
+        start_response('200 OK', [('Content-Type', 'text/plain')])
+        return [b"Hello World!"]
