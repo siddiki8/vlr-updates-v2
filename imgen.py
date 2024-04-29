ee69ce22bce020f224677082c3e22a11c8e0ca76
Cooper <cooperlewschool@gmail.com>
2024-04-28 18:02:33 -0700

    pillow branch

---
+++ imgen.py
@@ -0,0 +1,270 @@
+from PIL import Image, ImageFont, ImageDraw
+import random
+import os
+import requests
+from io import BytesIO
+from vlrbeta import MatchInfo, Match
+from xml_grabber import XML_links
+
+match_link = 'https://www.vlr.gg/314644/g2-esports-vs-mibr-champions-tour-2024-americas-stage-1-w4'
+
+
+xlinks = XML_links(match_link)
+m = Match(match_link, xlinks)
+match_info = MatchInfo(m)
+        
+print(match_info)
+print(match_info.event_name)
+print(match_info.round)
+print(match_info.map_info)
+print(match_info.team_info)
+
+
+# Construct final twitter image
+
+class Twimage:
+
+    def __init__(self, match_info):
+        self.event = match_info.event_name
+        self.round = match_info.round
+        self.map_info = match_info.map_info
+        self.team_info = match_info.team_info
+        #self.mvp = match_info.mvp_info
+        self.mvp2 = match_info.mvp_info
+        self.mvp = {
+
+            "name": self.mvp2['player_name'],
+            "K/D": self.mvp2['kills'] + "/" + self.mvp2['deaths'] + "/" + self.mvp2['assists'],
+            "ACS": self.mvp2['mvp_acs'],
+            "+/-": self.mvp2['pm'],
+            "HSP": self.mvp2['hsp'],
+            "agent": self.mvp2['agent'],
+            "img": self.mvp2['image']
+
+        } 
+        self.template = Image.new("RGBA", (1600, 900), (0, 0, 0, 0))
+        self.font = ImageFont.truetype("Assets\MISC\RussoOne-Regular.ttf", 100)
+        team_names = [team_name for team_name in self.team_info.keys()]
+        self.team1 = team_names[0]
+        self.team2 = team_names[1]
+        self.mvp_guy_link = self.mvp['img']
+
+#Function to change a url into an image
+
+    def urlpull(self, url):
+        team_logo = requests.get(url)
+        return Image.open(BytesIO(team_logo.content)).convert("RGBA")
+        # true white appearing as black
+
+# Choose random background image (stages) and add final box and MVP box
+
+    def create_background(self):
+
+        stages = os.listdir("Assets/STAGES")
+        image_path = ("Assets/STAGES/" + str(random.choice(stages)))
+        print(image_path)
+        image_to_paste = Image.open(image_path).convert("RGBA")
+        self.template.paste(image_to_paste, (0,0,1600,900), mask=image_to_paste)
+        
+        boiler_path = ("Assets\MISC\FINAL_BOX_MVP_CARD.png")
+        boiler_to_paste = Image.open(boiler_path).convert("RGBA")
+        self.template.paste(boiler_to_paste, (0,0,1600,900), mask=boiler_to_paste)
+
+# Add team logos and final match score
+
+    def final_score(self):
+        
+        
+
+        logo1_url = self.team_info[self.team1]["logo"]
+        logo2_url = self.team_info[self.team2]["logo"]
+
+        logo1_img = self.urlpull(logo1_url)
+        logo2_img = self.urlpull(logo2_url)
+
+        logo1_img.thumbnail((130, 130), reducing_gap=2.0)
+        self.template.paste(logo1_img, (87, 283), mask=logo1_img)
+
+        logo2_img.thumbnail((130, 130), reducing_gap=2.0)
+        self.template.paste(logo2_img, (523, 283), mask=logo2_img)
+
+        draw = ImageDraw.Draw(self.template)
+
+        start_x = 216
+        end_x = 520
+        start_y = 283
+        end_y = 420
+
+        matchscore_text = str(self.team_info[self.team1]['maps_won']) + " - " + str(self.team_info[self.team2]['maps_won'])
+
+        text_width = draw.textlength(matchscore_text, font=self.font)
+        center_x = (end_x - start_x) / 2 + start_x
+        center_y = (end_y - start_y) / 1.4 + start_y
+
+        draw.text((center_x - text_width / 2, center_y - 100), matchscore_text, font=self.font, fill="white")
+
+        
+        
+# Add each map icon for the maps that were played and add boxes to them (adjust for series length) and add map scores 
+
+    def add_maps(self):
+        
+        base_space_val = 100
+        map_count = len(self.map_info)
+        space_increment = base_space_val//map_count
+        start_coords = [81, 410]
+        end_coords = [81, (820 - space_increment)]
+        vertical_space = (end_coords[1] - start_coords[1])
+        negative_space = space_increment * (map_count)
+        
+        
+        map_box = Image.open("Assets\MISC\map_box_transparent.png").convert("RGBA")
+
+        map_box_width = (map_box.size[0])
+        map_box_height = (vertical_space - negative_space) // map_count
+
+        resized_map_box = map_box.resize((map_box_width, map_box_height))
+        first_gap = space_increment
+        map_box_gap = (space_increment + map_box_height)
+
+        for check_TBD in self.map_info:
+            if check_TBD == 'TBD':
+                del self.map_info['TBD']
+                break
+
+        j = 0
+        for current_map in self.map_info:
+
+
+            map_dir = "Assets\MAPS\Loading_Screen_" + current_map + ".png"
+            map_image = Image.open(map_dir).convert("RGBA")
+
+            resized_map_image = map_image.resize((map_box_width//3, map_box_height))
+            self.template.paste(resized_map_image, (73, (start_coords[1] + (map_box_gap)*(j)) + first_gap*(j+1)), mask=resized_map_image)
+
+            j += 1
+
+        for i in range(map_count):
+            
+            self.template.paste(resized_map_box, (73, (start_coords[1] + (map_box_gap)*(i)) + first_gap*(i+1)), mask=resized_map_box)
+        
+        text_indent  = map_box_height//10
+        text_size = (map_box_height - 6*(text_indent))
+        
+
+        font = ImageFont.truetype("Assets\MISC\RussoOne-Regular.ttf", text_size)
+        draw = ImageDraw.Draw(self.template)
+
+        W = resized_map_box.size[0]
+
+        logo_size = (text_size)
+
+        logo1_url = self.team_info[self.team1]["logo"]
+        logo2_url = self.team_info[self.team2]["logo"]
+
+        logo1_img = self.urlpull(logo1_url)
+        logo2_img = self.urlpull(logo2_url)
+
+        logo1_img.thumbnail((logo_size, logo_size), reducing_gap=2.0)
+        logo2_img.thumbnail((logo_size, logo_size), reducing_gap=2.0)
+
+        k = 0
+        for map in self.map_info:
+
+            score  = self.map_info[map]['score'][0] + " - " + self.map_info[map]['score'][1]
+
+            _, _, w, _ = draw.textbbox((0, 0), score, font=font)
+            text_offset = ((W - w)/2)
+            draw.text((73 + text_offset, (2.5*text_indent + start_coords[1] + (map_box_gap)*(k)) + first_gap*(k+1)), score, font=font, fill="white")
+            TEXT_X_POS = int(73 + text_offset)
+
+            for team in self.team_info:
+                
+                if (team) == (self.team1):
+
+                    KRU_X_POSITION = 73 + int(text_offset) - int(text_offset)//3
+                    GAP_TEXT = TEXT_X_POS - (KRU_X_POSITION + logo1_img.size[0])
+                    self.template.paste(logo1_img, (73 + int(text_offset) - int(text_offset)//3, (3*text_indent + start_coords[1] + (map_box_gap)*(k)) + first_gap*(k+1)), mask=logo1_img)
+
+                else:
+
+                    self.template.paste(logo2_img, (73 + int(text_offset) + w + GAP_TEXT, (3*text_indent + start_coords[1] + (map_box_gap)*(k)) + first_gap*(k+1)), mask=logo2_img)
+
+            k += 1
+            
+       
+
+
+# Add MVP image and stats and agent
+
+    
+    def add_mvp(self):
+
+        start_coords = [757, 700]
+        end_coords = [1031, 700]
+        draw = ImageDraw.Draw(self.template)
+        text_size = 21
+        
+
+        mvp_agent = self.mvp['agent']
+        mvp_name = self.mvp['name']
+
+        i = 0
+        for stat in self.mvp:
+
+            font = ImageFont.truetype("Assets\MISC\RussoOne-Regular.ttf", text_size)
+
+            if self.mvp[stat] == (mvp_name):
+
+               
+                name_coords = [758, 234]
+                #name_end_coords = [1020, 234]
+                text_name_construction = str((self.mvp[stat]))
+                font = ImageFont.truetype("Assets\MISC\RussoOne-Regular.ttf", 40)
+                _, _, w, _ = draw.textbbox((0, 0), text_name_construction, font=font)
+                #name_centered = (name_coords[0] + ((name_end_coords[0]) - name_coords[0] - w)/2)
+                draw.text((name_coords[0], name_coords[1]), text_name_construction, font=font, fill='white')
+                i += 1
+                continue
+
+            if self.mvp[stat] == (mvp_agent):
+                i += 1
+                continue
+
+            text_construction = str(list(self.mvp.keys())[list(self.mvp.values()).index(self.mvp[stat])]) + ":  " + str(self.mvp[stat])
+            _, _, w, _ = draw.textbbox((0, 0), text_construction, font=font)
+            
+            if i == 1:
+                draw.text((start_coords[0], start_coords[1] + 50*((i-1))) , text_construction, font = font, fill='white')
+            if i == 2:
+                draw.text((end_coords[0] - w, start_coords[1] + 50*((i-2))) , text_construction, font = font, fill='white')
+            if i == 3:
+                draw.text((start_coords[0], start_coords[1] + 50*((i-2))) , text_construction, font = font, fill='white')
+            if i == 4:
+                draw.text((end_coords[0] - w, start_coords[1] + 50*((i-3))) , text_construction, font = font, fill='white')
+            
+            i += 1
+        #AGENTS\Skye_Artwork_Full.png
+        agent_image = Image.open("Assets\AGENTS\\" + mvp_agent + "_Artwork_Full.png").convert("RGBA") 
+        agent_image.thumbnail((1500, 1500), reducing_gap=2.0)
+
+        self.template.paste(agent_image, (850, -20), mask=agent_image)
+
+        mvp_img = self.urlpull(self.mvp_guy_link)
+        resized_mvp_img = mvp_img.resize((250, 272), reducing_gap= 2.0)
+        self.template.paste(resized_mvp_img, (770, 310), mask=resized_mvp_img)
+
+        self.template.show()
+
+# Construct final image    
+
+    def construct_image(self):
+        pass
+
+
+t = Twimage(match_info)
+
+t.create_background()
+t.final_score()
+t.add_maps()
+t.add_mvp()
--- vlrbeta.py
+++ vlrbeta.py
@@ -12,38 +12,45 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
+from xml_grabber import XML_links
+
+xlinks = XML_links('https://www.vlr.gg/312779/gen-g-vs-sentinels-champions-tour-2024-masters-madrid-gf/?game=all&tab=overview')
+
 
 class Match:
-    def __init__(self, link):
+    def __init__(self, link, XML_links):
+
+        self.top_name = XML_links.xpaths["top"]["name"]
+        self.bot_name = XML_links.xpaths["bottom"]["name"]
+        self.top_acs = XML_links.xpaths["top"]["acs"]
+        self.bot_acs = XML_links.xpaths["bottom"]["acs"]
+        self.top_agent = XML_links.xpaths["top"]["agent"]
+        self.bot_agent = XML_links.xpaths["bottom"]["agent"]
+        self.top_url = XML_links.xpaths["top"]["url"]
+        self.bot_url = XML_links.xpaths["bottom"]["url"]
+        self.top_rating = XML_links.xpaths["top"]["rating"]
+        self.bot_rating = XML_links.xpaths["bottom"]["rating"]
+        self.top_kills = XML_links.xpaths["top"]["kills"]
+        self.bot_kills = XML_links.xpaths["bottom"]["kills"]
+        self.top_kills = XML_links.xpaths["top"]["kills"]
+        self.bot_kills = XML_links.xpaths["bottom"]["kills"]
+        self.top_deaths = XML_links.xpaths["top"]["deaths"]
+        self.bot_deaths = XML_links.xpaths["bottom"]["deaths"]
+        self.top_assists = XML_links.xpaths["top"]["assists"]
+        self.bot_assists = XML_links.xpaths["bottom"]["assists"]
+        self.top_pm = XML_links.xpaths["top"]["pm"]
+        self.bot_pm = XML_links.xpaths["bottom"]["pm"]
+        self.top_hsp = XML_links.xpaths["top"]["hsp"]
+        self.bot_hsp = XML_links.xpaths["bottom"]["hsp"]
+
+
         self.link = link
         self.match = self.requester(link)
-        self.team_info = {
-            'team1': self.get_team_info(1),
-            'team2': self.get_team_info(2)
-        }
-        self.map_info = self.mapscore()
-        self.mvp_info = self.get_mvp()
 
     @staticmethod
     def requester(url):
         htmlText = requests.get(url)
         return bs(htmlText.content, "lxml")
-
-    def map(self):
-        maps = [i.span.text.strip().replace('\n', '').replace('\t', '').replace('PICK', '') for i in
-                self.match.find_all('div', class_='map')]
-        return maps
-
-    def event(self):
-        event = self.match.find('div', style='font-weight: 700;').text.strip()
-        return event.replace('\n', '').replace('\t', '')
-
-    def round(self):
-        round = self.match.find('div', class_='match-header-event-series').text.strip()
-        return round.replace('\n', '').replace('\t', '')
-
-    def scores(self):
-        return [i.text.strip() for i in self.match.find_all('div', class_='score')]
 
     def get_team_logo_hl(self, url):
 
@@ -72,14 +79,15 @@ class Match:
         # Now the website should be in dark mode, and you can scrape it as needed
         logo_link = wait.until(EC.presence_of_element_located(
             (By.XPATH, '/html/body/div[5]/div[1]/div/div[1]/div[1]/div[1]/div/img'))).get_attribute('src')
+        
         # Don't forget to close the driver when you're done
         driver.quit()
+        
 
         return logo_link
 
     def get_team_info(self, mod):
-        team_url = 'https://www.vlr.gg' + self.match.find('a', class_=f"match-header-link wf-link-hover mod-{mod}")[
-            'href']
+        team_url = 'https://www.vlr.gg' + self.match.find('a', class_=f"match-header-link wf-link-hover mod-{mod}")['href']
         team_link = self.requester(team_url)
         team_name = team_link.find('h1', class_='wf-title').text.strip()
         try:
@@ -97,57 +105,128 @@ class Match:
             elif logo_link == '/img/vlr/tmp/vlr.png':
                 logo_link = 'https://www.vlr.gg/img/vlr/tmp/vlr.png'
 
-        return {"team_name": team_name, "twitter": twitter, "logo_link": logo_link}
+        return team_name, twitter, logo_link
 
+    def map(self):
+        maps = [i.span.text.strip().replace('\n', '').replace('\t', '').replace('PICK', '') for i in self.match.find_all('div', class_='map')]
+        return maps
+
+    def event(self):
+        event = self.match.find('div', style='font-weight: 700;').text.strip()
+        return event.replace('\n', '').replace('\t', '')
+
+    def round(self):
+        round = self.match.find('div', class_='match-header-event-series').text.strip()
+        return round.replace('\n', '').replace('\t', '')
+
+    def scores(self):
+        return [i.text.strip() for i in self.match.find_all('div', class_='score')]
+
+    def leftteam(self):
+        left_team_name, _, _ = self.get_team_info(1)
+        return left_team_name
+
+    def lefttwitter(self):
+        _, left_twitter, _ = self.get_team_info(1)
+        return left_twitter
+
+    def righttwitter(self):
+        _, right_twitter, _ = self.get_team_info(2)
+        return right_twitter
+
+    def rightteam(self):
+        right_team_name, _, _ = self.get_team_info(2)
+        return right_team_name
 
     def mapscore(self):
-        map_info = {}  # Initialize map_info as an empty dictionary
+        mapscore = []
         maps = self.map()
         score = self.scores()
         for i in range(len(maps)):
-            winner = self.team_info['team1']['team_name'] if int(score[i*2]) > int(score[i*2+1]) else self.team_info['team2']['team_name']
-            map_info[maps[i]] = {'score': score[i*2:i*2+2], 'winner': winner}  # Add map information to map_info
-
-        # Update team_info with maps won
-        self.team_info['team1']['maps_won'] = sum(1 for map_info in map_info.values() if map_info['winner'] == self.team_info['team1']['team_name'])
-        self.team_info['team2']['maps_won'] = sum(1 for map_info in map_info.values() if map_info['winner'] == self.team_info['team2']['team_name'])
-
-        return map_info
+            mapscore.append((maps[i], score[i*2:i*2+2]))
+        return mapscore
 
     def get_mvp(self):
-        acs = self.match.find_all('span', class_='stats-sq')
-        swag = [col for row in acs for col in row.find_all('span', class_='side mod-side mod-both') if col]
-        nlist = ['https://www.vlr.gg' + name.find('a', href=True)['href'] for name in self.match.find_all('td', class_='mod-player')]
+     
+        if float(self.top_rating) > float(self.bot_rating):
 
-        maplen = self.match.find_all('div', class_='match-header-vs-note')[1].text.lstrip()[2:]
-        acs1 = re.sub(r'\D', '', str(swag[1 if maplen == '1' else 31]))
-        acs2 = re.sub(r'\D', '', str(swag[16 if maplen == '1' else 46]))
+            playername = self.top_name
+            mvpacs = self.top_acs
+            agent = self.top_agent
+            pfp_link = self.top_url
+            hsp = self.top_hsp
+            pm = self.top_pm
+            kills = self.top_kills
+            deaths = self.top_deaths
+            assists = self.top_assists
 
-        mvpname = nlist[10 if acs1 > acs2 else 15]
-        mvpacs = acs1 if acs1 > acs2 else acs2
+        else:
 
-        player = self.requester(mvpname)
-        playername = player.find('h1', class_='wf-title').text.strip().replace('\t', '').replace(' ', '')
-        agent = player.find('img', style='display: block; width: 24px; height: 24px; background: #ccc; border-radius: 2px; image-rendering: -webkit-optimize-contrast;')['alt']
-        mvptwitter = player.find('a', style="margin-top: 3px; display: block;").text.strip() if player.find('a', style="margin-top: 3px; display: block;") else None
+            playername = self.bot_name
+            mvpacs = self.bot_acs
+            agent = self.bot_agent
+            pfp_link = self.bot_url
+            hsp = self.bot_hsp
+            pm = self.bot_pm
+            kills = self.bot_kills
+            deaths = self.bot_deaths
+            assists = self.bot_assists
+
+        player = self.requester(self.top_url)
         pfp_link = player.find('div', class_='wf-avatar mod-player').find('img')['src']
         if pfp_link.startswith('//'):
             pfp_link = 'https:' + pfp_link
         elif pfp_link == "/img/base/ph/sil.png":
             pfp_link = 'https://www.vlr.gg/img/base/ph/sil.png'
-
-        return {'player_name': playername, 'mvp_acs': mvpacs, 'agent': agent, 'twitter': mvptwitter, 'pic_link': pfp_link}
+        return {'player_name': playername, 'mvp_acs': mvpacs, 'agent': agent, 'image': pfp_link, 'hsp': hsp, 'pm': pm, 'kills': kills, 'deaths': deaths, 'assists': assists}
 
     def __str__(self):
-        event_name = self.event()
-        round_name = self.round()
-        team1_twitter = self.team_info['team1']['twitter'] or self.team_info['team1']['team_name']
-        team2_twitter = self.team_info['team2']['twitter'] or self.team_info['team2']['team_name']
-        team1_maps_won = self.team_info['team1']['maps_won']
-        team2_maps_won = self.team_info['team2']['maps_won']
-        mvp_twitter = self.mvp_info['twitter'] or self.mvp_info['player_name']
+        output = ""
+        s = self.mapscore()
+        rightteam = self.rightteam()
+        leftteam = self.leftteam()
+        map = self.map()
+        ltwitter = self.lefttwitter()
+        rtwitter = self.righttwitter()
+        lwinner, rwinner = 0, 0
+        for i in range(len(s)):
+            if int(s[i][1][0]) > int(s[i][1][1]):
+                output += f"{leftteam} defeat {rightteam} {s[i][1][0]} to {s[i][1][1]} on {map[i]}.\n"
+                lwinner += 1
+            else:
+                output += f"{rightteam} defeat {leftteam} {s[i][1][1]} to {s[i][1][0]} on {map[i]}.\n"
+                rwinner += 1
+        winner = f"{ltwitter or leftteam} {lwinner}-{rwinner} {rtwitter or rightteam}"
+        final_score = f"{self.event()} {self.round()}\n{winner.lstrip()}\n{output}"
+        return final_score if len(final_score) <= 280 else f"{self.event()} {self.round()}\n\n{winner.lstrip()}\n\n{output}"
 
-        return f"{event_name} ({round_name})\n{team1_twitter} {team1_maps_won} - {team2_maps_won} {team2_twitter}\nMVP: {mvp_twitter}"
+#m = Match("https://www.vlr.gg/336088/shopify-rebellion-vs-misu-s-qts-game-changers-2024-north-america-series-2-sf")
+
+
+class MatchInfo:
+    def __init__(self, match):
+        self.event_name = match.event()
+        self.round = match.round()
+
+        team1_name, team1_twitter, team1_logo = match.get_team_info(1)
+        team2_name, team2_twitter, team2_logo = match.get_team_info(2)
+
+        self.team_info = {
+            team1_name: {'twitter': team1_twitter, 'logo': team1_logo, 'maps_won': 0},
+            team2_name: {'twitter': team2_twitter, 'logo': team2_logo, 'maps_won': 0}
+        }
+
+        self.map_info = {}  # Initialize map_info as an empty dictionary
+        for map_name, scores in match.mapscore():
+            winner = team1_name if int(scores[0]) > int(scores[1]) else team2_name
+            self.team_info[winner]['maps_won'] += 1
+            self.map_info[map_name] = {'score': scores, 'winner': winner}  # Add map information to map_info
+        self.mvp_info = match.get_mvp()
+
+    def __str__(self):
+        team1_name, team1_info = list(self.team_info.items())[0]
+        team2_name, team2_info = list(self.team_info.items())[1]
+        return f"{team1_info['twitter'] or team1_name} {team1_info['maps_won']}-{team2_info['maps_won']} {team2_info['twitter'] or team2_name}"
 
 def live_matches():
     match = requester('https://www.vlr.gg/matches')
@@ -169,7 +248,12 @@ def live_matches():
 def is_completed(matchurl):
     game = requester(matchurl)
     return game.find('div', class_='match-header-vs-note').text.strip() == 'final'
-
 def requester(url):
   htmlText = requests.get(url)
   return bs(htmlText.content, "lxml")
+
+#print(live_matches())
+
+#m = Match('https://www.vlr.gg/324469/kpi-gaming-vs-case-esports-challengers-league-2024-spain-rising-split-1-playoffs-gf')
+#mi = MatchInfo(m)
+
