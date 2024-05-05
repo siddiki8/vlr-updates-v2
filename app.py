871cd2a62f3341559848c6ff7ff65d7aea7f34d0
Abdullah Siddiki <abdullahsiddiki48@gmail.com>
2024-05-04 23:07:16 -0400

    deployment setup

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ def run_job():
     while True:
         clist = []
         matchlist = vlr.live_matches()      # Get list of live matches
-        print("These are the current live matches: ", matchlist)
 
         for match in matchlist:             # Iterate through list of live matches and add to live_list if not already in it
             if match not in live_list:
@@ -26,17 +25,9 @@ def run_job():
                 live_list.remove(match)   # If match is completed, remove from live_list
                 clist.append(match)       # Add match to list of completed matches
 
-        if len(clist) == 0:
-            print("No matches completed")
-        else:
-            print("These matches have completed: ", clist)
 
         for match in clist:                # Iterate through list of completed matches
-            print("tweeting match: " + match)
             t.tweet_match(match)  # Tweet completed match
-            print("Tweeted match: " + match)
-        print("Sleeping for 60 seconds")
-        print("Zzz...")
         time.sleep(60)
 
 if __name__ == "__main__":
